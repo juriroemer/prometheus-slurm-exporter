2ea7e676db0a0b36f8a789c8142db41eec7b53d5
Juri <tuxwurf@gmail.com>
2021-05-20 16:51:55 +0200

    fixed squeue command

--- jobs.go
+++ jobs.go
@@ -38,7 +38,8 @@ func ParseJobsMetrics(input []byte) map[string]*JobsMetrics {
 	linesUniq := RemoveDuplicates(lines)
 
 	for _, line := range linesUniq[1:] {
-		job := strings.Fields(line)
+		job := strings.Split(line, ",")
+
 		jobId := job[0]
 
 		jobs[jobId] = &JobsMetrics{"", "", "", "", 0, 0, ""}
@@ -93,7 +94,7 @@ func RunningTimeToSeconds(input string) uint64 {
 
 func JobData() []byte {
 	//make this cleaner?
-	cmd := exec.Command("squeue", "--states=R", "-o", "%i%P%j%u%T%S%M%D%N")
+	cmd := exec.Command("squeue", "--states=R", "-o", "%i,%P,%j,%u,%T,%S,%M,%D,%N")
 	out, err := cmd.Output()
 	if err != nil {
 		log.Fatal(err)
